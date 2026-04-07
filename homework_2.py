9d14fd1e9751f8f81a18bc73638e8f2bd9e174fa
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-04-07 19:22:29 +0200

    the square of a number option 2

--- homework_2.py
+++ homework_2.py
@@ -7,3 +7,8 @@ number_1 = input('Enter a number: ')
 print(number_1)
 result_square = int(number_1) ** 2
 print(result_square)
+# option 2
+number_2 = input('Enter a number: ')
+print(number_2)
+result2_square = math.pow(float(number_2),2)
+print(result2_square)
