6834719e738e3609a42bb4623cb14dc194140b1b
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-04-07 21:01:51 +0200

    the average of three numbers

--- homework_2.py
+++ homework_2.py
@@ -10,5 +10,11 @@ print(result_square)
 # option 2
 number_2 = input('Enter a number: ')
 print(number_2)
-result2_square = math.pow(float(number_2),2)
-print(result2_square)
+result1_square = math.pow(int(number_2),2)
+print(result1_square)
+
+# 2. the average of three numbers
+number_3, number_4, number_5 = map(int, input('Enter three numbers: ').split(","))
+print(number_3, number_4, number_5)
+result2 = (number_3 + number_4 + number_5) / 3
+print(result2)
