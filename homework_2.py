3b3a4243b184b8cced8a9bf5815ca7586eafba7a
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-04-08 00:59:28 +0200

    discount calculation

--- homework_2.py
+++ homework_2.py
@@ -8,15 +8,21 @@ result_square = int(number_1) ** 2
 print('The square of a number: ', result_square,'\n')
 # option 2
 number_2 = input('Enter a number: ')
-result1_square = math.pow(int(number_2),2)
-print('The square of a number:', result1_square,'\n')
+result_1_square = math.pow(int(number_2),2)
+print('The square of a number:', result_1_square,'\n')
 
 # 2. the average of three numbers
 number_3, number_4, number_5 = map(int, input('Enter three numbers: ').split(","))
-result2 = (number_3 + number_4 + number_5) / 3
-print('The average of three numbers:', result2,'\n')
+result_2 = (number_3 + number_4 + number_5) / 3
+print('The average of three numbers:', result_2,'\n')
 
 # 3. conversion of minutes into hours
 numer_6 = input('Enter the number of minutes: ')
-result3_1, result3_2 = divmod(int(numer_6), 60)
-print(result3_1, 'hours', result3_2, 'minutes', '\n')
+result_3_1, result_3_2 = divmod(int(numer_6), 60)
+print(result_3_1, 'hours', result_3_2, 'minutes', '\n')
+
+# 4. discount calculation
+numer_7 = input('Enter the price: ')
+numer_8 = input('Enter the discount: ')
+result_4 = int(numer_7) - int(numer_7) * int(numer_8) / 100
+print('Price after discount: ', result_4, '\n')
