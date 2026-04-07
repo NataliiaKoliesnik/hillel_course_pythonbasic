7efcd9d8186e3962f54979482693f4a726e66f39
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-04-08 00:36:41 +0200

    conversion of minutes into hours

--- homework_2.py
+++ homework_2.py
@@ -4,17 +4,19 @@ import math
 # 1. the square of a number
 # option 1
 number_1 = input('Enter a number: ')
-print(number_1)
 result_square = int(number_1) ** 2
-print(result_square)
+print('The square of a number: ', result_square,'\n')
 # option 2
 number_2 = input('Enter a number: ')
-print(number_2)
 result1_square = math.pow(int(number_2),2)
-print(result1_square)
+print('The square of a number:', result1_square,'\n')
 
 # 2. the average of three numbers
 number_3, number_4, number_5 = map(int, input('Enter three numbers: ').split(","))
-print(number_3, number_4, number_5)
 result2 = (number_3 + number_4 + number_5) / 3
-print(result2)
+print('The average of three numbers:', result2,'\n')
+
+# 3. conversion of minutes into hours
+numer_6 = input('Enter the number of minutes: ')
+result3_1, result3_2 = divmod(int(numer_6), 60)
+print(result3_1, 'hours', result3_2, 'minutes', '\n')
