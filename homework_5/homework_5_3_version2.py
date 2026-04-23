f3f668171c6eb34c9f47d486f7345329d20ca3a1
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-04-23 23:47:48 +0200

    added homework_5_3_version2

---
+++ homework_5/homework_5_3_version2.py
@@ -0,0 +1,21 @@
+# hashtag
+my_str = input(f'Enter a string: ')
+from string import digits, ascii_letters, punctuation, ascii_uppercase
+
+# 3. each word starts with a capital letter
+my_str = my_str.title()
+
+# 1. no symbols or spaces
+my_str_1 = str()
+for i in my_str:
+    if i not in punctuation and i not in ' ':
+        my_str_1 += i
+
+# adding a hashtag
+my_str_1 = '#' + my_str_1
+
+# 2. the length of the hashtag must not exceed 140 characters
+if len(my_str_1) > 140:
+    my_str_1 = my_str_1[:140]
+
+print(f'{my_str_1}')
