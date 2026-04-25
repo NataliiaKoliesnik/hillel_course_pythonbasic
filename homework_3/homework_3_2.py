501e158c06248dddfeea48c87b353021bd628e5d
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-04-26 00:55:07 +0200

    some changes have been made to files homework_3_2 and homework_3_3 according to the feedback

--- homework_3/homework_3_2.py
+++ homework_3/homework_3_2.py
@@ -1,45 +1,18 @@
 # moving the last element of a list from the end to the beginning
 
-# code verification - example 1
-lst_1 = [12, 3, 4, 10]
-if lst_1:
-    if len(lst_1) == 1:
-        print(lst_1)
+# example 1
+#lst = [12, 3, 4, 10]
+# example 2
+#lst = [1]
+# example 3
+#lst = []
+# example 4
+lst = [12, 3, 4, 10, 8]
+if lst:
+    if len(lst) == 1:
+        print(lst)
     else:
-        lst = lst_1.insert(0,lst_1.pop())
-        print(lst_1)
+        lst.insert(0, lst.pop())
+        print(lst)
 else:
-    print(lst_1)
-
-# code verification - example 2
-lst_2 = [1]
-if lst_2:
-    if len(lst_2) == 1:
-        print(lst_2)
-    else:
-        lst = lst_2.insert(0,lst_2.pop())
-        print(lst_2)
-else:
-    print(lst_2)
-
-# code verification - example 3
-lst_3 = []
-if lst_3:
-    if len(lst_3) == 1:
-        print(lst_3)
-    else:
-        lst = lst_3.insert(0,lst_3.pop())
-        print(lst_3)
-else:
-    print(lst_3)
-
-# code verification - example 4
-lst_4 = [12, 3, 4, 10, 8]
-if lst_4:
-    if len(lst_4) == 1:
-        print(lst_4)
-    else:
-        lst = lst_4.insert(0,lst_4.pop())
-        print(lst_4)
-else:
-    print(lst_4)
+    print(lst)
--- homework_3/homework_3_3.py
+++ homework_3/homework_3_3.py
@@ -1,96 +1,28 @@
 # splitting one list into two lists
 
-# code verification - example 1
-lst_1 = [1, 2, 3, 4, 5, 6]
-if lst_1:
-    if len(lst_1) % 2 == 0:
-        m = len(lst_1) // 2
-        lst_1 = [lst_1[:m], lst_1[m:len(lst_1)]]
+# example 1
+lst = [1, 2, 3, 4, 5, 6]
+# example 2
+#lst = [1, 2, 3]
+# example 3
+#lst = [1, 2, 3, 4, 5]
+# example 4
+#lst = [1]
+# example 5
+#lst = []
+if lst:
+    if len(lst) % 2 == 0:
+        m = len(lst) // 2
+        lst_1 = [lst[:m], lst[m:len(lst)]]
         print(lst_1)
     else:
-        if len(lst_1) == 1:
-            lst_1 = [lst_1,[]]
+        if len(lst) == 1:
+            lst_1 = [lst,[]]
             print(lst_1)
         else:
-            m = len(lst_1) // 2 + 1
-            lst_1 = [lst_1[:m], lst_1[m:len(lst_1)]]
+            m = len(lst) // 2 + 1
+            lst_1 = [lst[:m], lst[m:len(lst)]]
             print(lst_1)
 else:
     lst_1 = [[],[]]
-    print(lst_1)
-
-# code verification - example 2
-lst_2 = [1, 2, 3]
-if lst_2:
-    if len(lst_2) % 2 == 0:
-        n = len(lst_2) // 2
-        lst_2 = [lst_2[:n], lst_2[n:len(lst_2)]]
-        print(lst_2)
-    else:
-        if len(lst_2) == 1:
-            lst_2 = [lst_2,[]]
-            print(lst_2)
-        else:
-            n = len(lst_2) // 2 + 1
-            lst_2 = [lst_2[:n], lst_2[n:len(lst_2)]]
-            print(lst_2)
-else:
-    lst_2 = [[],[]]
-    print(lst_2)
-
-# code verification - example 3
-lst_3 = [1, 2, 3, 4, 5]
-if lst_3:
-    if len(lst_3) % 2 == 0:
-        l = len(lst_3) // 2
-        lst_3 = [lst_3[:l], lst_3[l:len(lst_3)]]
-        print(lst_3)
-    else:
-        if len(lst_3) == 1:
-            lst_3 = [lst_3,[]]
-            print(lst_3)
-        else:
-            l = len(lst_3) // 2 + 1
-            lst_3 = [lst_3[:l], lst_3[l:len(lst_3)]]
-            print(lst_3)
-else:
-    lst_3 = [[],[]]
-    print(lst_3)
-
-# code verification - example 4
-lst_4 = [1]
-if lst_4:
-    if len(lst_4) % 2 == 0:
-        k = len(lst_4) // 2
-        lst_4 = [lst_4[:k], lst_4[k:len(lst_4)]]
-        print(lst_4)
-    else:
-        if len(lst_4) == 1:
-            lst_4 = [lst_4,[]]
-            print(lst_4)
-        else:
-            k = len(lst_4) // 2 + 1
-            lst_4 = [lst_4[:k], lst_4[k:len(lst_4)]]
-            print(lst_4)
-else:
-    lst_4 = [[],[]]
-    print(lst_4)
-
-# code verification - example 5
-lst_5 = []
-if lst_5:
-    if len(lst_5) % 2 == 0:
-        p = len(lst_5) // 2
-        lst_5 = [lst_5[:p], lst_5[p:len(lst_5)]]
-        print(lst_5)
-    else:
-        if len(lst_5) == 1:
-            lst_5 = [lst_5,[]]
-            print(lst_5)
-        else:
-            p = len(lst_5) // 2 + 1
-            lst_5 = [lst_5[:p], lst_5[p:len(lst_5)]]
-            print(lst_5)
-else:
-    lst_5 = [[],[]]
-    print(lst_5)
+    print(lst)
