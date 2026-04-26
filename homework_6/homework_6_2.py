9a2e3933f12de6addc8698aadb842976b79b522b
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-04-26 16:11:00 +0200

    added file homework_6_3, completed task 6_3, and added comments to file homework_6_2

--- homework_6/homework_6_2.py
+++ homework_6/homework_6_2.py
@@ -1,27 +1,30 @@
 # Конвертер із числа в дату
 
 from string import digits
+
 num = input('Введіть число, яке > або = 0 і < 86640000: ')
-flag = 0
+
+# перевірка на число, в тому числі мінусове
+flag_num = 0
 for i in num[1:]:
-    if num[0] == '-' or num[0] in digits:  # перевірка на число, в тому числі мінусове
-        if i not in digits:
-            flag += 1
+    if num[0] == '-' or num[0] in digits:  # чи починається наша строка на - чи число
+        if i not in digits:  # чи є всі інші елементи стоки числами
+            flag_num += 1
             break
     else:
-        flag += 1
+        flag_num += 1
         break
 
-if flag == 0:
+if flag_num == 0: # чи працюємо ми з числом
     if 0 <= int(num) < 86640000: # перевірка на відповідність діапазону
-        num_day, remainder_num_day = divmod(int(num), (24 * 60 * 60))
-        num_hours, remainder_num_hours = divmod(int(remainder_num_day), (60 * 60))
-        num_minutes, num_seconds = divmod(int(remainder_num_hours), 60)
-        if 0 <= num_hours < 10:
+        num_day, remainder_num_day = divmod(int(num), (24 * 60 * 60)) # знаходимо дні і залишок
+        num_hours, remainder_num_hours = divmod(int(remainder_num_day), (60 * 60)) # знаходимо години і залишок
+        num_minutes, num_seconds = divmod(int(remainder_num_hours), 60) # знаходимо хвилини і секунди
+        if 0 <= num_hours < 10: # формат 00:00:00 для годин
             num_hours = str(num_hours).zfill(2)
-        if 0 <= num_minutes < 10:
+        if 0 <= num_minutes < 10: # формат 00:00:00 для хвилин
             num_minutes = str(num_minutes).zfill(2)
-        if 0 <= num_seconds < 10:
+        if 0 <= num_seconds < 10: # формат 00:00:00 для секунд
             num_seconds = str(num_seconds).zfill(2)
         print(f'{num_day} днів, {num_hours}:{num_minutes}:{num_seconds}')
     else:
---
+++ homework_6/homework_6_3.py
@@ -0,0 +1,33 @@
+# the product of the digits of an integer until it becomes <= 9
+
+from string import digits
+
+num = input(f'Enter a number : ')
+
+# checking whether a value is an integer, including negative numbers
+flag_num = 0
+for i in num[1:]:
+    if num[0] == '-' or num[0] in digits: # does the string start with - or a digit
+        if i not in digits: # are all other characters digits
+            flag_num += 1
+            break
+    else:
+        flag_num += 1
+        break
+
+result = int(num)
+
+if flag_num == 0: # are we working with a number
+    while abs(result) > 9: # check the condition <= 9
+        if str(result)[0] == '-': # does the new number start with -
+            product = -1
+            for i in str(result)[1:]:
+                product *= int(i)
+        else:
+            product = 1
+            for i in str(result):
+                product *= int(i)
+        result = product
+    print(result)
+else:
+    print('You entered a non-number')
