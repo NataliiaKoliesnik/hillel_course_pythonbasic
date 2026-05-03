cf0f2e59e37ce0cc6c494175100d79dc099bdbfd
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-05-03 23:17:39 +0200

    the file homework_8_1 has been added and task 8_1 has been completed

---
+++ homework_8/homework_8_1.py
@@ -0,0 +1,16 @@
+# a function that adds 1 to a number obtained from a given list and outputs it as a list
+
+def add_one(some_list):
+    my_str = ""
+    for i in some_list:
+        my_str += str(i) # converting a list to a string
+    new_list = []
+    # converting a string to a number, adding 1, then converting it back to a string and to a list
+    for i in str(int(my_str) + 1):
+        new_list.append(int(i)) #
+    return new_list # outputting the new list
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
