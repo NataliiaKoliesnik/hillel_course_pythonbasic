c627c8dc0332434c49ede26db72705b4e6b7644b
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-05-20 21:07:39 +0200

    added homework_10 folder, created homework_10_1 file, and completed task 10_1: a generator function that uses yield

---
+++ homework_10/homework_10_1.py
@@ -0,0 +1,25 @@
+# генераторна функція з використанням оператора yield, яка повертатиме
+# по одному члену числової послідовності, закон якої задається за допомогою функції користувача
+
+def my_pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    new_elem = begin
+    count = 1
+    while count <= end:
+         yield new_elem
+         new_elem = func(new_elem)
+         count += 1
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, my_pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
