32808f64160cc787835a1079c079f956d5d71c8d
NataliiaKoliesnik <kolesnyknataliya@gmail.com>
2026-05-20 15:12:17 +0200

    minor updates to homework_9_1 and homework_9_2

--- homework_9/homework_9_1.py
+++ homework_9/homework_9_1.py
@@ -10,5 +10,6 @@ def popular_words (text: str, words: list[str]) -> dict[str, int]:
     new_text = text.lower().split() # we convert our text into a list of lowercase words
     # we store in a dictionary the checked word and the number of its occurrences in our text
     return {i :new_text.count(i) for i in words}
+
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
 print('OK')
--- homework_9/homework_9_2.py
+++ homework_9/homework_9_2.py
@@ -1,10 +1,8 @@
 # a function that returns the difference between max and min for a variable number of arguments
 
 def difference (*args: float | int) -> float | int:
-    if args:
-        return round((max(args) - min(args)), 2)
-    else:
-        return 0
+    return round((max(args) - min(args)), 2) if args else 0
+
 assert difference(1, 2, 3) == 2, 'Test1'
 assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
